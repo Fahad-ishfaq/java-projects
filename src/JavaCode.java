fd64b29294b8d891099988e531a89be95d2c84f2
Fahad-ishfaq <sardarfahadashfaq@gmail.com>
2022-11-29 23:55:51 +0500

    Min max and time conversion in java

--- src/JavaCode.java
+++ src/JavaCode.java
@@ -1,29 +1,63 @@
 import modal.Whois;
 
-import java.io.File;
-import java.math.BigDecimal;
-import java.math.BigInteger;
-import java.math.RoundingMode;
-import java.time.LocalDateTime;
 import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class JavaCode {
 
     public static final String PATTERN_COMPRESSED_ARIN_NET_RANGE = "\\(\\S+\\) \\S+ - \\S+";
 
-    public void test(boolean bool) {
-        if (bool) {
-            System.out.println("bool");
-        } else {
-            System.out.println("none");
+    public static void minMax(List<Integer> arr) {
+        long min = Long.MAX_VALUE;
+        System.out.println(min);
+
+        long max = Long.MIN_VALUE;
+        System.out.println(max);
+
+        for (int i = 0; i < arr.size(); i ++) {
+            long oneSum = 0;
+            for (int j = i; j < arr.size() -1 + i; j ++) {
+                int k = j;
+                if (j >= arr.size()) {
+                    k = j - arr.size() ;
+                }
+                oneSum += arr.get(k);
+            }
+            if (oneSum < min) {
+                min = oneSum;
+            }
+            if (oneSum > max) {
+                max = oneSum;
+            }
         }
+        System.out.println(min + " " + max);
     }
 
-    public  void main(String[] args) {
+    public static String timeConversion(String s) {
+        // Write your code here
+        String[] splitString = s.split(":");
+        int hours = Integer.parseInt(splitString[0]);
+        String hourString = splitString[0];
+        if (s.endsWith("PM")) {
+            if (hours < 12) {
+                hours += 12;
+                hourString = String.valueOf(hours);
+            }
+        } else if (s.endsWith("AM")) {
+            if (hours == 12) {
+                hourString = "00";
+            }
+        }
+        splitString[0] = hourString;
+        String newString = String.join(":", splitString);
+        newString = newString.replace("AM", "");
+        newString = newString.replace("PM", "");
+        return newString;
 
-        this.test(true);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("run");
+        minMax(List.of(new Integer[]{426980153, 354802167, 142980735, 968217435, 734892650}));
     }
 }
 
